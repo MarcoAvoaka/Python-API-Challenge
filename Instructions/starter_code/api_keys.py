ed169f09fc19b319f909a7129a208ecf752fe27d
DIDIER AVOAKA <didieravoaka@DIDIERs-MBP.lan>
2021-11-17 18:33:58 -0500

    Updates

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8e8caadeece3d74e6ee3ab363ff8ba4e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCu55uwpFWpp2dhZLKnKRYWs5IUuwaLrcw"
